7be6ee2f0d0d383939036af812f6534f1b9ecd86
Daniel Havlíček <daniel.havlicek@braiins.cz>
2025-04-17 09:33:49 +0200

    bmc: Add NoPassword and Session layers #BOS-3050

---
+++ bmc/src/web/no_password.rs
@@ -0,0 +1,131 @@
+// Copyright (C) 2025  Braiins Systems s.r.o.
+
+use crate::session;
+use crate::web::session::extract_cookies;
+
+use axum_extra::extract::cookie::Cookie;
+use futures::Future;
+use http::{HeaderValue, Request, Response};
+use tower::{Layer, Service};
+
+use std::pin::Pin;
+use std::sync::Arc;
+use std::task::{Context, Poll};
+
+const DEFAULT_USER_NAME: &str = "root";
+const DEFAULT_PASSWORD: &str = "";
+
+#[derive(Debug)]
+pub struct NoPassword<U, V>
+where
+    U: session::Manager,
+{
+    session_manager: Arc<U>,
+    service: V,
+}
+
+impl<U, V> NoPassword<U, V>
+where
+    U: session::Manager,
+{
+    pub fn new(session_manager: Arc<U>, service: V) -> Self {
+        Self {
+            session_manager,
+            service,
+        }
+    }
+}
+
+impl<U: session::Manager, V: Clone> Clone for NoPassword<U, V> {
+    fn clone(&self) -> Self {
+        Self {
+            session_manager: self.session_manager.clone(),
+            service: self.service.clone(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct NoPasswordLayer<T>
+where
+    T: session::Manager,
+{
+    session_manager: Arc<T>,
+}
+
+impl<T: session::Manager> Clone for NoPasswordLayer<T> {
+    fn clone(&self) -> Self {
+        Self {
+            session_manager: self.session_manager.clone(),
+        }
+    }
+}
+
+impl<T> NoPasswordLayer<T>
+where
+    T: session::Manager,
+{
+    pub fn new(session_manager: Arc<T>) -> Self {
+        Self { session_manager }
+    }
+}
+
+impl<U, V> Layer<V> for NoPasswordLayer<U>
+where
+    U: session::Manager,
+{
+    type Service = NoPassword<U, V>;
+
+    fn layer(&self, inner: V) -> Self::Service {
+        NoPassword::new(self.session_manager.clone(), inner)
+    }
+}
+
+impl<T, S, ReqBody, ResBody> Service<Request<ReqBody>> for NoPassword<T, S>
+where
+    T: session::Manager + Send + Sync + 'static,
+    S: Service<Request<ReqBody>, Response = Response<ResBody>> + Clone + Send + Sync + 'static,
+    ReqBody: Send + 'static,
+    ResBody: Send + 'static,
+    S::Future: Send + 'static,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.service.poll_ready(cx)
+    }
+
+    fn call(&mut self, mut req: Request<ReqBody>) -> Self::Future {
+        let mut service = self.service.clone();
+        let session_manager = self.session_manager.clone();
+
+        Box::pin(async move {
+            let cookies: Vec<Cookie<'_>> = extract_cookies(req.headers()).collect();
+
+            let mut response_set_cookie = None;
+
+            if session_manager.find(&cookies).await.is_err() {
+                _ = session_manager
+                    .login(DEFAULT_USER_NAME.to_owned(), DEFAULT_PASSWORD.to_owned())
+                    .await
+                    .map(|cookie| {
+                        if let Ok(parsed_cookie) = cookie.to_string().parse::<HeaderValue>() {
+                            response_set_cookie = Some(parsed_cookie.clone());
+
+                            req.headers_mut()
+                                .append(http::header::COOKIE, parsed_cookie);
+                        }
+                    });
+            }
+
+            let mut resp = service.call(req).await?;
+
+            if let Some(cookie) = response_set_cookie {
+                resp.headers_mut().append(http::header::SET_COOKIE, cookie);
+            }
+            Ok(resp)
+        })
+    }
+}
---
+++ bmc/src/web/session.rs
@@ -0,0 +1,162 @@
+// Copyright (C) 2025  Braiins Systems s.r.o.
+
+use crate::session::{self};
+
+use axum_extra::extract::cookie::Cookie;
+use futures::Future;
+use http::{HeaderMap, HeaderValue, Request, Response, header};
+use tower::{Layer, Service};
+
+use std::pin::Pin;
+use std::sync::Arc;
+use std::task::{Context, Poll};
+
+#[derive(Debug)]
+pub struct Session<U, V>
+where
+    U: session::Manager,
+{
+    session_manager: Arc<U>,
+    service: V,
+}
+
+impl<U, V> Session<U, V>
+where
+    U: session::Manager,
+{
+    pub fn new(session_manager: Arc<U>, service: V) -> Self {
+        Self {
+            session_manager,
+            service,
+        }
+    }
+}
+
+impl<U: session::Manager, V: Clone> Clone for Session<U, V> {
+    fn clone(&self) -> Self {
+        Self {
+            session_manager: self.session_manager.clone(),
+            service: self.service.clone(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct SessionLayer<T>
+where
+    T: session::Manager,
+{
+    session_manager: Arc<T>,
+}
+
+impl<T: session::Manager> Clone for SessionLayer<T> {
+    fn clone(&self) -> Self {
+        Self {
+            session_manager: self.session_manager.clone(),
+        }
+    }
+}
+
+impl<T> SessionLayer<T>
+where
+    T: session::Manager,
+{
+    pub fn new(session_manager: Arc<T>) -> Self {
+        Self { session_manager }
+    }
+}
+
+impl<U, V> Layer<V> for SessionLayer<U>
+where
+    U: session::Manager,
+{
+    type Service = Session<U, V>;
+
+    fn layer(&self, inner: V) -> Self::Service {
+        Session::new(self.session_manager.clone(), inner)
+    }
+}
+
+impl<T, S, ReqBody, ResBody> Service<Request<ReqBody>> for Session<T, S>
+where
+    T: session::Manager + Send + Sync + 'static,
+    S: Service<Request<ReqBody>, Response = Response<ResBody>> + Clone + Send + Sync + 'static,
+    ReqBody: Send + 'static,
+    ResBody: Send + 'static,
+    S::Future: Send + 'static,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.service.poll_ready(cx)
+    }
+
+    fn call(&mut self, mut req: Request<ReqBody>) -> Self::Future {
+        let mut service = self.service.clone();
+        let session_manager = self.session_manager.clone();
+
+        Box::pin(async move {
+            let cookies: Vec<Cookie<'_>> = extract_cookies(req.headers()).collect();
+
+            session_manager
+                .find(&cookies)
+                .await
+                .ok()
+                .and_then(|session| req.extensions_mut().insert(session));
+
+            service.call(req).await
+        })
+    }
+}
+
+pub(crate) fn extract_cookies(
+    headers: &HeaderMap<HeaderValue>,
+) -> impl Iterator<Item = Cookie<'_>> {
+    headers
+        .get_all(header::COOKIE)
+        .iter()
+        .flat_map(|hdr| {
+            let s = String::from_utf8_lossy(hdr.as_bytes());
+            Cookie::split_parse_encoded(s)
+        })
+        .filter_map(Result::ok)
+}
+
+#[cfg(test)]
+mod tests {
+
+    use header::{ACCEPT_ENCODING, CONTENT_LENGTH, COOKIE};
+
+    use super::*;
+
+    #[test]
+    fn test_extract_cookies() {
+        let mut header_map = HeaderMap::new();
+
+        let headers = vec![
+            (COOKIE, "session_id=gVZIvHtgCYYfbxXa"),
+            (CONTENT_LENGTH, "320"),
+            (ACCEPT_ENCODING, "gzip"),
+            (COOKIE, "test=kjfdsQFKSowowFFW; test2=fdsdfQgWHd"),
+        ];
+
+        for (key, value) in headers {
+            header_map.append(
+                key,
+                value.parse().expect("BUG: failed to parse header value"),
+            );
+        });
+
+        let cookies = extract_cookies(&header_map).collect::<Vec<Cookie>>();
+
+        let expected_cookies = vec![
+            Cookie::new("session_id", "gVZIvHtgCYYfbxXa"),
+            Cookie::new("test", "kjfdsQFKSowowFFW"),
+            Cookie::new("test2", "fdsdfQgWHd"),
+        ];
+
+        assert_eq!(cookies, expected_cookies);
+    }
+}
