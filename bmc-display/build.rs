2efd0f2fd5dbd279dacc2e39c0d0f643623783fa
Viktor Burian <viktor.burian@braiins.cz>
2025-04-22 16:55:55 +0200

    bmc-display: Update slint ui path in build.rs #BOS-3043

--- bmc-display/build.rs
+++ bmc-display/build.rs
@@ -4,7 +4,7 @@ use anyhow::Context;
 
 fn main() -> anyhow::Result<()> {
     slint_build::compile_with_config(
-        "assets/ui/main.slint",
+        "ui/main.slint",
         slint_build::CompilerConfiguration::new()
             .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer),
     )
