8aee7b4c546850fa770560ae22aaa7f8153a329b
Jiří Andras <jiri.andras@braiins.cz>
2025-05-07 10:30:03 +0200

    bmc-main: Impl BmcManager for Mock #BOS-3042

--- bmc-mock/src/lib.rs
+++ bmc-mock/src/lib.rs
@@ -11,38 +11,15 @@ use tokio as _;
 use tokio as _;
 use tracing::info;
 
-mod cli;
-mod mockfs;
+pub mod cli;
+pub mod manager;
+pub mod mock_index;
+pub mod mockfs;
 mod session;
 
+use bmc_display as _;
+use bmc_mock_display as _;
+
+use slint as _;
+
 pub use mockfs::MockFs;
-
-#[derive(thiserror::Error, Debug)]
-pub enum Error {
-    #[error(transparent)]
-    Io(#[from] std::io::Error),
-}
-
-#[derive(Clone, Debug)]
-pub struct MockManager {
-    pub session_manager: MockSessionManager,
-}
-
-#[async_trait::async_trait]
-impl BmcManager for MockManager {
-    type Error = Error;
-    type SessionManager = MockSessionManager;
-
-    fn session_manager(&self) -> Self::SessionManager {
-        self.session_manager.clone()
-    }
-
-    fn version(&self) -> String {
-        format!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"))
-    }
-
-    async fn set_password(&self, password: Option<String>) -> Result<(), Self::Error> {
-        info!("Setting password to {:?}", password);
-        Ok(())
-    }
-}
---
+++ bmc-mock/src/manager.rs
@@ -0,0 +1,62 @@
+// Copyright (C) 2025  Braiins Systems s.r.o.
+
+use bmc_platform::BmcPlatform;
+use std::{path::Path, time::Duration};
+use tracing::info;
+
+use crate::mockfs::MockFs;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+}
+
+#[derive(Debug)]
+pub struct Manager {
+    mockfs: MockFs,
+    pub session_manager: MockSessionManager,
+}
+
+impl Manager {
+    #[must_use]
+    pub fn new(mockfs: MockFs) -> Self {
+        Self { mockfs }
+    }
+}
+
+#[async_trait::async_trait]
+impl bmc::BmcManager for Manager {
+    type Error = Error;
+    type SessionManager = MockSessionManager;
+
+    fn version(&self) -> String {
+        "0.1.0".to_owned()
+    }
+
+    fn platform(&self) -> BmcPlatform {
+        BmcPlatform::BraiinsBmc
+    }
+
+    async fn upgrade(&self, keep_settings: bool, _upgrade_image_path: &Path) -> anyhow::Result<()> {
+        info!(
+            "Performing system upgrade (keep_settings={})...",
+            keep_settings
+        );
+        tokio::time::sleep(Duration::from_secs(10)).await;
+        Ok(())
+    }
+
+    async fn check_and_remove_upgrade_marker(&self) -> bool {
+        self.mockfs.upgrade_result().exists()
+    }
+
+    fn session_manager(&self) -> Self::SessionManager {
+        self.session_manager.clone()
+    }
+
+    async fn set_password(&self, password: Option<String>) -> Result<(), Self::Error> {
+        info!("Setting password to {:?}", password);
+        Ok(())
+    }
+}
