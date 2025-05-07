a2d8cf896349acbe0833bfad288a0d08da96ec67
Jiří Andras <jiri.andras@braiins.cz>
2025-05-07 10:24:57 +0200

    bmc-main: Extend BmcManager trait
    - add methods to get platform, do upgrade and check if upgrade was
    successful
    - #BOS-3042

--- bmc/src/manager.rs
+++ bmc/src/manager.rs
@@ -1,11 +1,24 @@
 // Copyright (C) 2025  Braiins Systems s.r.o.
 
+use std::{fmt::Debug, path::Path};
+
+use bmc_platform::BmcPlatform;
+
 #[async_trait::async_trait]
-pub trait BmcManager: Sync + Send + 'static {
+pub trait BmcManager: Sync + Send + 'static + Debug {
     type SessionManager: crate::session::Manager;
     type Error: std::error::Error + Send + Sync;
 
     fn version(&self) -> String;
+
+    fn platform(&self) -> BmcPlatform;
+
+    async fn upgrade(&self, keep_settings: bool, upgrade_image_path: &Path) -> anyhow::Result<()>;
+
+    // Checks if a system upgrade was performed
+    async fn check_and_remove_upgrade_marker(&self) -> bool;
+
     fn session_manager(&self) -> Self::SessionManager;
+
     async fn set_password(&self, password: Option<String>) -> Result<(), Self::Error>;
 }
