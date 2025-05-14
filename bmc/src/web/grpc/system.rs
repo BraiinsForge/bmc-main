b06b6eadf09118641888f480ece067ee0cad96b5
Jiří Andras <jiri.andras@braiins.cz>
2025-05-14 12:53:28 +0200

    bmc-main: Implement gRPC service for get/set timezone #BOS-3163

--- bmc/src/web/grpc/system.rs
+++ bmc/src/web/grpc/system.rs
@@ -1,14 +1,20 @@
 // Copyright (C) 2025  Braiins Systems s.r.o.
 
+use std::str::FromStr;
 use std::sync::Arc;
 
 use bmc_grpc::web::{
-    SetPasswordRequest, SetPasswordResponse,
-    system_service_server::SystemService as GrpcSystemService,
+    GetTimezoneListResponse, GetTimezoneResponse, SetPasswordRequest, SetPasswordResponse,
+    SetTimezoneRequest, system_service_server::SystemService as GrpcSystemService,
 };
+use tonic::{Code, Request};
+use tonic_types::{ErrorDetails, StatusExt};
 use tracing::warn;
 
-use crate::{BmcManager, session::Manager as SessionManager, web::session::extract_session};
+use super::GrpcError;
+use crate::{
+    BmcManager, session::Manager as SessionManager, time::Timezone, web::session::extract_session,
+};
 
 #[derive(Clone)]
 pub(crate) struct SystemService<T, S>
@@ -16,7 +22,7 @@ where
     T: BmcManager,
     S: SessionManager,
 {
-    bmc_manager: Arc<T>,
+    manager: Arc<T>,
     session_manager: Arc<S>,
 }
 
@@ -25,9 +31,9 @@ where
     T: BmcManager,
     S: SessionManager,
 {
-    pub(crate) fn new(bmc_manager: Arc<T>, session_manager: Arc<S>) -> Self {
+    pub(crate) fn new(manager: Arc<T>, session_manager: Arc<S>) -> Self {
         Self {
-            bmc_manager,
+            manager,
             session_manager,
         }
     }
@@ -57,7 +63,7 @@ where
                 ))
             })?;
 
-        self.bmc_manager
+        self.manager
             .set_password(request.new_password)
             .await
             .map_err(|err| tonic::Status::internal(format!("Failed to set password: {err}")))?;
@@ -67,5 +73,58 @@ where
         }
 
         Ok(tonic::Response::new(SetPasswordResponse {}))
+    }
+
+    async fn get_timezone(
+        &self,
+        _request: Request<()>,
+    ) -> Result<tonic::Response<GetTimezoneResponse>, tonic::Status> {
+        Ok(tonic::Response::new(GetTimezoneResponse {
+            timezone: Some(into_grpc_timezone(&self.manager.timezone())),
+        }))
+    }
+
+    async fn get_timezone_list(
+        &self,
+        _request: Request<()>,
+    ) -> Result<tonic::Response<GetTimezoneListResponse>, tonic::Status> {
+        let timezones = self
+            .manager
+            .timezone_list()
+            .map(|tz| into_grpc_timezone(&tz))
+            .collect();
+        Ok(tonic::Response::new(GetTimezoneListResponse { timezones }))
+    }
+
+    async fn set_timezone(
+        &self,
+        request: Request<SetTimezoneRequest>,
+    ) -> Result<tonic::Response<()>, tonic::Status> {
+        let value = request.into_inner().id;
+        let timezone = Timezone::from_str(&value).map_err(|_| {
+            tonic::Status::with_error_details(
+                Code::InvalidArgument,
+                GrpcError::BadRequest.to_string(),
+                ErrorDetails::with_bad_request_violation("timezone", "invalid timezone variant"),
+            )
+        })?;
+
+        self.manager.set_timezone(timezone).await.map_err(|e| {
+            warn!("Failed to set timezone: {}", e);
+            tonic::Status::internal("Unexpected error occured when settign timezone")
+        })?;
+
+        Ok(tonic::Response::new(()))
+    }
+}
+
+fn into_grpc_timezone(timezone: &Timezone) -> bmc_grpc::web::Timezone {
+    bmc_grpc::web::Timezone {
+        id: timezone.normalize_iana(),
+        label: timezone.iana.to_owned(),
+        offset: timezone
+            .current_timezone_offset()
+            .map(|offset| offset.to_string())
+            .unwrap_or_default(),
     }
 }
