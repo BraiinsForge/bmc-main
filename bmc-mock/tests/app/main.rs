4c4c447e269e18523a294a5c97c3e8c5740e1441
Daniel Havlíček <daniel.havlicek@braiins.cz>
2025-05-15 13:33:26 +0200

    bmc: Fix clippy errors in tests #BOS-3050

--- bmc-mock/tests/app/main.rs
+++ bmc-mock/tests/app/main.rs
@@ -1,3 +1,10 @@
 // Copyright (C) 2025  Braiins Systems s.r.o.
 
+use async_trait as _;
+use axum_extra as _;
+use rand as _;
+use thiserror as _;
+use time as _;
+use tracing as _;
+
 mod test_utils;
