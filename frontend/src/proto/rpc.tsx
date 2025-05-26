3faae45d388464a7308b6b5ed3bc3f24994ea1a6
Josef Kubíček <josef.kubicek@braiins.cz>
2025-05-26 15:02:06 +0300

    proto: Move buf config to root for better intellij plugin support
    https://github.com/bufbuild/intellij-buf/issues/243#issuecomment-2243347272

--- frontend/src/proto/rpc.tsx
+++ frontend/src/proto/rpc.tsx
@@ -1,16 +1,16 @@
 import { getClient } from './transport';
 
 // Export root-level PBs
-export * from './gen/web/authentication_pb';
-export * from './gen/web/metadata_pb';
-export * from './gen/web/system_pb';
-export * from './gen/web/upgrade_pb';
+export * from './gen/authentication_pb';
+export * from './gen/metadata_pb';
+export * from './gen/system_pb';
+export * from './gen/upgrade_pb';
 
 // Services
-import { AuthenticationService } from './gen/web/authentication_pb';
-import { MetadataService } from './gen/web/metadata_pb';
-import { SystemService } from './gen/web/system_pb';
-import { UpgradeService } from './gen/web/upgrade_pb';
+import { AuthenticationService } from './gen/authentication_pb';
+import { MetadataService } from './gen/metadata_pb';
+import { SystemService } from './gen/system_pb';
+import { UpgradeService } from './gen/upgrade_pb';
 
 // Utils
 export const rpc = {
