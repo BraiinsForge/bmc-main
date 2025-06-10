e87b8bfcdc17c1777309b7a6204dbda5e61ad3b1
Jiří Andras <jiri.andras@braiins.cz>
2025-06-10 09:22:21 +0200

    bmc-grpc: Add scan wifi gRPC function #BOS-3041

--- bmc/src/web/grpc/initial_setup.rs
+++ bmc/src/web/grpc/initial_setup.rs
@@ -1,18 +1,20 @@
 // Copyright (C) 2025  Braiins Systems s.r.o.
 
 use bmc_grpc::web::{
-    EncryptionType as GrpcEncryptionType, SetWifiRequest,
+    EncryptionType as GrpcEncryptionType, ScanWifiResponse, SetWifiRequest,
+    SignalStrength as GrpcSignalStrength, WifiNetwork,
     initial_setup_service_server::InitialSetupService as GrpcInitialSetupService,
 };
 use std::sync::Arc;
 use tonic::{Code, Request, Response, Status};
 use tonic_types::{ErrorDetails, StatusExt};
+use tracing::warn;
 
 use super::GrpcError;
 use crate::{
     BmcManager,
     initial_setup::{InitialSetup, SetupError},
-    manager::{EncryptionType, WifiNetworkConfig},
+    manager::{EncryptionType, SignalStrength, WifiNetworkConfig},
 };
 
 #[derive(Clone)]
@@ -34,6 +36,15 @@ where
             initial_setup,
         }
     }
+
+    async fn check_precondition(&self) -> Result<(), Status> {
+        if !self.manager.is_factory_default().await {
+            return Err(Status::failed_precondition(
+                "Function is only available when the device is in its factory default state.",
+            ));
+        }
+        Ok(())
+    }
 }
 
 #[async_trait::async_trait]
@@ -42,11 +53,7 @@ where
     T: BmcManager,
 {
     async fn set_wifi(&self, request: Request<SetWifiRequest>) -> Result<Response<()>, Status> {
-        if !self.manager.is_factory_default().await {
-            return Err(Status::failed_precondition(
-                "Initial setup is only available when the device is in its factory default state.",
-            ));
-        }
+        self.check_precondition().await?;
 
         let request = request.into_inner();
 
@@ -55,11 +62,32 @@ where
         match self.initial_setup.connect_to_wifi(config) {
             Ok(()) => Ok(Response::new(())),
             Err(e) => match e {
-                SetupError::InProgress => Err(Status::failed_precondition(
-                    "Initial setup is already being set up",
-                )),
+                SetupError::InProgress => {
+                    Err(Status::failed_precondition("Initial setup is in progress"))
+                }
             },
         }
+    }
+
+    async fn scan_wifi(&self, _request: Request<()>) -> Result<Response<ScanWifiResponse>, Status> {
+        self.check_precondition().await?;
+
+        let available_wifi = self.manager.wifi_scan().await.map_err(|e| {
+            warn!("Failed to scan WiFi networks: {}", e);
+            Status::internal("Failed to scan WiFi networks")
+        })?;
+
+        Ok(Response::new(ScanWifiResponse {
+            networks: available_wifi
+                .into_iter()
+                .filter(|wifi| wifi.signal_strength() != SignalStrength::Offline)
+                .map(|wifi| WifiNetwork {
+                    signal_strength: into_grpc_signal_strength(wifi.signal_strength()) as i32,
+                    ssid: wifi.ssid,
+                    encryption_type: into_encryption_type(wifi.encryption_type) as i32,
+                })
+                .collect(),
+        }))
     }
 }
 
@@ -90,5 +118,26 @@ fn try_into_encryption_type(value: GrpcEncryptionType) -> Result<EncryptionType,
         GrpcEncryptionType::Wpa12 => Ok(EncryptionType::Wpa1_2),
         GrpcEncryptionType::Wpa2 => Ok(EncryptionType::Wpa2),
         GrpcEncryptionType::Wpa23 => Ok(EncryptionType::Wpa2_3),
+    }
+}
+
+fn into_encryption_type(value: EncryptionType) -> GrpcEncryptionType {
+    match value {
+        EncryptionType::None => GrpcEncryptionType::None,
+        EncryptionType::Wep => GrpcEncryptionType::Wep,
+        EncryptionType::WepShared => GrpcEncryptionType::WepShared,
+        EncryptionType::Wpa => GrpcEncryptionType::Wpa,
+        EncryptionType::Wpa1_2 => GrpcEncryptionType::Wpa12,
+        EncryptionType::Wpa2 => GrpcEncryptionType::Wpa2,
+        EncryptionType::Wpa2_3 => GrpcEncryptionType::Wpa23,
+    }
+}
+
+fn into_grpc_signal_strength(value: SignalStrength) -> GrpcSignalStrength {
+    match value {
+        SignalStrength::Offline => GrpcSignalStrength::Unspecified, // Offline WiFi is filtered out
+        SignalStrength::Low => GrpcSignalStrength::Weak,
+        SignalStrength::Fair => GrpcSignalStrength::Moderate,
+        SignalStrength::Excellent => GrpcSignalStrength::Strong,
     }
 }
