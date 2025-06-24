156357a113bdfb898b58b693cdfbf3b1cbae9cf7
Daniel Havlíček <daniel.havlicek@braiins.cz>
2025-06-24 13:44:58 +0200

    bmc-shared: Add "ii-net" crate #BOS-3252

---
+++ bmc-shared/ii-net/src/lib.rs
@@ -0,0 +1,81 @@
+// Copyright (C) 2024  Braiins Systems s.r.o.
+//
+// This file is part of Braiins Open-Source Initiative (BOSI).
+//
+// BOSI is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Please, keep in mind that we may also license BOSI or any part thereof
+// under a proprietary license. For more information on the terms and conditions
+// of such proprietary license or if you have any other questions, please
+// contact us at opensource@braiins.com.
+
+use std::fmt::{Display, Formatter, Result};
+
+pub mod wifi;
+
+/// Format of the IP report packet. Available substitutions: "${MAC}", "${IP}", "${HOSTNAME}".
+/// "${IP},${MAC}" is default since that is what stock IP reporter tool expects.
+pub const DEFAULT_BROADCAST_FORMAT: &str = "${IP},${MAC}";
+
+#[derive(Clone, Debug, Default, PartialEq, Eq)]
+pub struct MacAddr {
+    a: u8,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+    f: u8,
+}
+
+impl MacAddr {
+    pub const DELIMITER: &'static str = ":";
+
+    #[must_use]
+    #[expect(clippy::many_single_char_names)]
+    pub fn new(a: u8, b: u8, c: u8, d: u8, e: u8, f: u8) -> Self {
+        Self { a, b, c, d, e, f }
+    }
+}
+
+impl Display for MacAddr {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(
+            f,
+            "{:02x}{}{:02x}{}{:02x}{}{:02x}{}{:02x}{}{:02x}",
+            self.a,
+            Self::DELIMITER,
+            self.b,
+            Self::DELIMITER,
+            self.c,
+            Self::DELIMITER,
+            self.d,
+            Self::DELIMITER,
+            self.e,
+            Self::DELIMITER,
+            self.f
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn mac_addr_string_format() {
+        let mac_addr = MacAddr::new(0x01, 0x23, 0x45, 0x67, 0x89, 0xAB);
+
+        assert_eq!(&mac_addr.to_string(), "01:23:45:67:89:ab");
+    }
+}
---
+++ bmc-shared/ii-net/src/wifi.rs
@@ -0,0 +1,187 @@
+// Copyright (C) 2024  Braiins Systems s.r.o.
+//
+// This file is part of Braiins Open-Source Initiative (BOSI).
+//
+// BOSI is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Please, keep in mind that we may also license BOSI or any part thereof
+// under a proprietary license. For more information on the terms and conditions
+// of such proprietary license or if you have any other questions, please
+// contact us at opensource@braiins.com.
+
+use anyhow::Result;
+use strum_macros::{Display, EnumString};
+
+#[derive(Default, EnumString, Debug, Display, Eq, PartialEq, Clone)]
+pub enum WifiMode {
+    #[default]
+    Station,
+    Ap,
+}
+
+impl WifiMode {
+    pub fn to_uci_mode(&self) -> String {
+        match self {
+            WifiMode::Ap => "ap".to_string(),
+            WifiMode::Station => "sta".to_string(),
+        }
+    }
+
+    pub fn to_uci_network(&self) -> String {
+        match self {
+            WifiMode::Ap => "wifi_ap".to_string(),
+            WifiMode::Station => "wifi_sta".to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Clone, Copy, Default, Display, PartialOrd, Ord)]
+pub enum SignalStrength {
+    #[default]
+    Offline,
+    Low,
+    Fair,
+    Excellent,
+}
+
+impl SignalStrength {
+    pub fn new(signal: i32) -> Self {
+        match signal {
+            x if x == 0 => SignalStrength::Offline,
+            x if x >= -60 => SignalStrength::Excellent,
+            x if x >= -75 => SignalStrength::Fair,
+            _ => SignalStrength::Low,
+        }
+    }
+}
+
+#[derive(Debug, EnumString, PartialEq, Eq, PartialOrd, Ord, Default, Clone, Copy, Display)]
+pub enum EncryptionType {
+    #[default]
+    None,
+    Wep,
+    WepShared,
+    Wpa,
+    Wpa1_2,
+    Wpa2,
+    Wpa2_3,
+    Wpa3, // Currently not supported on BMM101
+}
+
+impl EncryptionType {
+    pub fn to_uci_str(&self) -> &str {
+        match self {
+            EncryptionType::None => "none",
+            EncryptionType::Wep => "wep",
+            EncryptionType::WepShared => "wep+shared",
+            EncryptionType::Wpa => "psk",
+            EncryptionType::Wpa1_2 => "psk-mixed",
+            EncryptionType::Wpa2 => "psk2",
+            EncryptionType::Wpa2_3 => "sae-mixed",
+            EncryptionType::Wpa3 => "sae",
+        }
+    }
+
+    pub fn from_uci_str(s: &str) -> Option<Self> {
+        match s {
+            "none" => Some(EncryptionType::None),
+            "wep" => Some(EncryptionType::Wep),
+            "wep+shared" => Some(EncryptionType::WepShared),
+            "psk" => Some(EncryptionType::Wpa),
+            "psk-mixed" => Some(EncryptionType::Wpa1_2),
+            "psk2" => Some(EncryptionType::Wpa2),
+            "sae-mixed" => Some(EncryptionType::Wpa2_3),
+            "sae" => Some(EncryptionType::Wpa3),
+            s => {
+                log::warn!("Encryption type not recognized: {s}");
+                None
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct WifiScanItem {
+    pub ssid: String,
+    pub signal_level: i32,
+    pub encryption_type: EncryptionType,
+}
+
+impl WifiScanItem {
+    pub fn new(ssid: String, signal_level: i32, encryption_type: EncryptionType) -> Self {
+        Self {
+            ssid,
+            signal_level,
+            encryption_type,
+        }
+    }
+
+    pub fn signal_strength(&self) -> SignalStrength {
+        SignalStrength::new(self.signal_level)
+    }
+}
+
+#[derive(Default, Debug, PartialEq, Clone, Eq)]
+pub struct WifiStatus {
+    pub enabled: bool,
+    pub configuration: Option<WifiConfiguration>,
+    pub sta_link_state: Option<WifiLinkState>,
+}
+
+#[derive(Default, Debug, PartialEq, Clone, Eq)]
+pub struct WifiConfiguration {
+    pub mode: WifiMode,
+    pub ssid: String,
+    pub encryption_type: EncryptionType,
+}
+
+#[derive(Default, Debug, PartialEq, Clone, Eq)]
+pub struct WifiLinkState {
+    pub ssid: String,
+    pub signal_level: i32,
+}
+
+impl WifiLinkState {
+    pub fn new(ssid: &str, signal_level: i32) -> Self {
+        Self {
+            ssid: ssid.to_string(),
+            signal_level,
+        }
+    }
+
+    pub fn signal_strength(&self) -> SignalStrength {
+        SignalStrength::new(self.signal_level)
+    }
+}
+
+#[async_trait::async_trait]
+pub trait WifiManager {
+    async fn status(&self) -> Result<WifiStatus>;
+    async fn scan(&self) -> Result<Vec<WifiScanItem>>;
+    async fn enable(&self, enable: bool) -> Result<()>;
+    async fn save_and_connect(
+        &self,
+        ssid: String,
+        password: Option<String>,
+        encryption: EncryptionType,
+    ) -> Result<()>;
+    async fn configure_ap_mode(
+        &self,
+        ssid: String,
+        password: Option<String>,
+        encryption: EncryptionType,
+    ) -> Result<()>;
+    async fn reset_config(&self) -> Result<()>;
+    async fn reload(&self) -> Result<()>;
+}
