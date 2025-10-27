318e0500fb4c3af0df1606c361f64e7bea804f8e
Jiří Andras <jiri.andras@braiins.cz>
2025-10-27 15:41:55 +0100

    bmc-shared: Add ii-dns crate from bos-main #BDK-46

---
+++ bmc-shared/ii-dns/src/hickory.rs
@@ -0,0 +1,135 @@
+// Copyright (C) 2025  Braiins Systems s.r.o.
+//
+// This file is part of Braiins Open-Source Initiative (BOSI).
+//
+// BOSI is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Please, keep in mind that we may also license BOSI or any part thereof
+// under a proprietary license. For more information on the terms and conditions
+// of such proprietary license or if you have any other questions, please
+// contact us at opensource@braiins.com.
+
+use std::{
+    fmt::Debug,
+    io::{Error, ErrorKind},
+    net::SocketAddr,
+    time::Duration,
+};
+
+use hickory_resolver::{
+    Resolver,
+    config::{
+        LookupIpStrategy, ResolveHosts, ResolverConfig, ResolverOpts, ServerOrderingStrategy,
+    },
+    name_server::{GenericConnector, TokioConnectionProvider},
+    proto::runtime::TokioRuntimeProvider,
+};
+use log::error;
+use rand::rng;
+use rand::seq::SliceRandom;
+
+use crate::ToHostAndPortTuple;
+
+pub struct HickoryResolverBuilder {
+    system_resolver: Option<Resolver<GenericConnector<TokioRuntimeProvider>>>,
+    google_resolver: Resolver<GenericConnector<TokioRuntimeProvider>>,
+}
+
+impl HickoryResolverBuilder {
+    pub fn new() -> Self {
+        Self {
+            system_resolver: Self::build_system_resolver(),
+            google_resolver: Self::build_google_resolver(),
+        }
+    }
+
+    fn get_resolver_options() -> ResolverOpts {
+        let mut opts = ResolverOpts::default();
+        opts.use_hosts_file = ResolveHosts::Always;
+        opts.edns0 = true;
+        opts.server_ordering_strategy = ServerOrderingStrategy::UserProvidedOrder;
+        opts.ip_strategy = LookupIpStrategy::Ipv4AndIpv6;
+        opts.negative_max_ttl = Some(Duration::ZERO);
+        opts.attempts = 1;
+        opts.cache_size = 0;
+
+        opts
+    }
+
+    fn build_system_resolver() -> Option<Resolver<GenericConnector<TokioRuntimeProvider>>> {
+        match Resolver::builder_tokio() {
+            Ok(resolver) => Some(resolver.with_options(Self::get_resolver_options()).build()),
+            Err(e) => {
+                error!("Cannot initialize system DNS resolver: {}", e);
+                None
+            }
+        }
+    }
+
+    fn build_google_resolver() -> Resolver<GenericConnector<TokioRuntimeProvider>> {
+        Resolver::builder_with_config(
+            ResolverConfig::default(),
+            TokioConnectionProvider::default(),
+        )
+        .with_options(Self::get_resolver_options())
+        .build()
+    }
+
+    pub async fn lookup_host_google<T>(
+        &self,
+        host: T,
+    ) -> Result<impl Iterator<Item = SocketAddr> + Send + use<T>, Error>
+    where
+        T: ToHostAndPortTuple + Debug + Clone + Send + Sync,
+    {
+        self.lookup_host_internal(self.google_resolver.clone(), host)
+            .await
+    }
+
+    pub async fn lookup_host_system<T>(
+        &self,
+        host: T,
+    ) -> Result<impl Iterator<Item = SocketAddr> + Send + use<T>, Error>
+    where
+        T: ToHostAndPortTuple + Debug + Clone + Send + Sync,
+    {
+        if let Some(resolver) = self.system_resolver.clone() {
+            Ok(self.lookup_host_internal(resolver, host).await?)
+        } else {
+            Err(Error::new(ErrorKind::Other, "Resolver not initialized"))
+        }
+    }
+
+    async fn lookup_host_internal<T>(
+        &self,
+        resolver: Resolver<GenericConnector<TokioRuntimeProvider>>,
+        host: T,
+    ) -> Result<impl Iterator<Item = SocketAddr> + Send + use<T>, Error>
+    where
+        T: ToHostAndPortTuple + Debug + Clone + Send + Sync,
+    {
+        let (host, port) = host.to_host_and_port_tuple();
+
+        let mut result: Vec<SocketAddr> = resolver
+            .lookup_ip(host)
+            .await
+            .map_err(|e| Error::new(ErrorKind::Other, e))?
+            .into_iter()
+            .map(move |addr| SocketAddr::new(addr, port))
+            .collect();
+        result.shuffle(&mut rng());
+
+        Ok(result.into_iter())
+    }
+}
---
+++ bmc-shared/ii-dns/src/lib.rs
@@ -0,0 +1,152 @@
+// Copyright (C) 2025  Braiins Systems s.r.o.
+//
+// This file is part of Braiins Open-Source Initiative (BOSI).
+//
+// BOSI is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Please, keep in mind that we may also license BOSI or any part thereof
+// under a proprietary license. For more information on the terms and conditions
+// of such proprietary license or if you have any other questions, please
+// contact us at opensource@braiins.com.
+
+use std::{
+    fmt::Debug,
+    io::{Error, ErrorKind},
+    net::SocketAddr,
+    path::Path,
+    time::Duration,
+};
+
+use log::{error, info, warn};
+use tokio::{
+    net::TcpStream,
+    time::{MissedTickBehavior, interval},
+};
+
+use crate::hickory::HickoryResolverBuilder;
+
+mod hickory;
+
+pub trait ToHostAndPortTuple {
+    fn to_host_and_port_tuple(&self) -> (String, u16);
+}
+
+impl<T: ToString> ToHostAndPortTuple for (T, u16) {
+    fn to_host_and_port_tuple(&self) -> (String, u16) {
+        (self.0.to_string(), self.1)
+    }
+}
+
+pub struct IiResolver;
+
+impl IiResolver {
+    const RETRY_DELAY: Duration = Duration::from_secs(1);
+    const TOKIO_RETRIES: u8 = 2;
+    const GOOGLE_DNS_FLAG_FILE_PATH: &str = "/etc/google-dns-fallback";
+
+    pub async fn lookup_host<T>(host: T) -> Result<impl Iterator<Item = SocketAddr> + Send, Error>
+    where
+        T: ToHostAndPortTuple + Debug + Clone + Send + Sync,
+    {
+        let mut interval = interval(Self::RETRY_DELAY);
+        interval.set_missed_tick_behavior(MissedTickBehavior::Delay);
+        for attempt in 1..=Self::TOKIO_RETRIES {
+            interval.tick().await;
+            match tokio::net::lookup_host(host.clone().to_host_and_port_tuple()).await {
+                Ok(iterator) => {
+                    info!("Host {:?} resolved by tokio resolver successfully", host);
+                    return Ok(iterator.collect::<Vec<SocketAddr>>().into_iter());
+                }
+                Err(e) => warn!(
+                    "Unable to resolve {:?} using tokio resovler (try: {}/{}): {}",
+                    &host,
+                    attempt,
+                    Self::TOKIO_RETRIES,
+                    e
+                ),
+            }
+        }
+
+        let hickory_resolver = HickoryResolverBuilder::new();
+        match hickory_resolver
+            .lookup_host_system(host.clone().to_host_and_port_tuple())
+            .await
+        {
+            Ok(iterator) => {
+                info!(
+                    "Host {:?} resolved by hickory system resolver successfully",
+                    host
+                );
+                return Ok(iterator.collect::<Vec<SocketAddr>>().into_iter());
+            }
+            Err(e) => warn!(
+                "Unable to resolve {:?} using hickory system resolver: {}",
+                &host, e
+            ),
+        }
+
+        if Path::new(Self::GOOGLE_DNS_FLAG_FILE_PATH).exists() {
+            match hickory_resolver
+                .lookup_host_google(host.clone().to_host_and_port_tuple())
+                .await
+            {
+                Ok(iterator) => {
+                    info!(
+                        "Host {:?} resolved by hickory google resolver successfully",
+                        host
+                    );
+                    return Ok(iterator.collect::<Vec<SocketAddr>>().into_iter());
+                }
+                Err(e) => warn!(
+                    "Unable to resolve {:?} using hickory google resolver: {}",
+                    &host, e
+                ),
+            }
+        }
+
+        let err_msg = format!("Failed to resolve {:?}", &host);
+        error!("{}", err_msg);
+        Err(Error::new(ErrorKind::Other, err_msg))
+    }
+
+    pub fn lookup_host_sync<T>(host: T) -> Result<impl Iterator<Item = SocketAddr> + Send, Error>
+    where
+        T: ToHostAndPortTuple + Debug + Clone + Send + Sync,
+    {
+        futures::executor::block_on(Self::lookup_host(host))
+    }
+}
+#[derive(Debug)]
+pub struct IiTcpStream;
+
+impl IiTcpStream {
+    pub async fn connect<T>(host: T) -> Result<TcpStream, Error>
+    where
+        T: ToHostAndPortTuple + Debug + Clone + Send + Sync,
+    {
+        let addrs: Vec<SocketAddr> = IiResolver::lookup_host(host).await?.collect();
+        let mut last_err = None;
+        for addr in addrs {
+            match TcpStream::connect(addr).await {
+                Ok(tcpstream) => {
+                    info!("TcpStream connected to {}", addr);
+                    return Ok(tcpstream);
+                }
+                Err(e) => last_err = Some(e),
+            }
+        }
+
+        Err(last_err.unwrap_or_else(|| Error::new(ErrorKind::Other, "This should not happen")))
+    }
+}
