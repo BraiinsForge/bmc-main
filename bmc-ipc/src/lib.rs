8031c4527992314e6e5cd30949315e8f19d5de14
Jiří Andras <jiri.andras@braiins.cz>
2025-12-08 09:41:03 +0100

    bmc-ipc: Add JsonProtocol implementation
    - implement JsonProtocol struct for JSON serialization
    - add unit tests for encoding and decoding
    - #BDK-141

--- bmc-ipc/src/lib.rs
+++ bmc-ipc/src/lib.rs
@@ -1,7 +1,9 @@
 // Copyright (C) 2025  Braiins Systems s.r.o.
 
 mod error;
+mod json_protocol;
 mod protocol;
 
 pub use error::ProtocolError;
+pub use json_protocol::JsonProtocol;
 pub use protocol::Protocol;
