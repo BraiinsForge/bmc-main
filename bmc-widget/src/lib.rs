d70e6f90a98e0851f4c271adb6501c81454c2a71
Jiří Andras <jiri.andras@braiins.cz>
2025-12-09 12:16:44 +0100

    bmc-widget: Add WidgetClient for IPC communication
    - add client.rs with WidgetClient struct
    - implement connect() using BMC_IPC_SOCKET env var
    - implement send/recv for AppMessage and WidgetMessage
    - add convenience methods send_ready() and send_error()
    - add unit tests for connection and message handling
    - #BDK-141

--- bmc-widget/src/lib.rs
+++ bmc-widget/src/lib.rs
@@ -1,7 +1,9 @@
 // Copyright (C) 2025  Braiins Systems s.r.o.
 
+mod client;
 mod error;
 mod manifest;
 
+pub use client::{ClientError, WidgetClient};
 pub use error::ManifestError;
 pub use manifest::{Author, Manifest, ParamDefinition, ParamType, SettingKey};
