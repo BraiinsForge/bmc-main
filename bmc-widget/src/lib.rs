02c23b4fce10a89d8f9f5f796251ff602dbc0137
Jiří Andras <jiri.andras@braiins.cz>
2025-12-10 13:44:32 +0100

    bmc-widget: Add IPC helper functions for widgets #BDK-141
    - connect_widget() handles socket connection and Init handshake
    - run_message_loop() processes SettingsUpdate and Shutdown messages

--- bmc-widget/src/lib.rs
+++ bmc-widget/src/lib.rs
@@ -2,8 +2,10 @@
 
 mod client;
 mod error;
+mod ipc;
 mod manifest;
 
 pub use client::{ClientError, WidgetClient};
 pub use error::ManifestError;
+pub use ipc::{connect_widget, run_message_loop};
 pub use manifest::{Author, Manifest, ParamDefinition, ParamType, SettingKey};
