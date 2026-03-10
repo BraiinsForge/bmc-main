bf6d0e40883da0f6e8d3230bcd1b295dd6200aa7
František Boháček <frantisek.bohacek@braiinsforge.com>
2026-03-10 18:37:11 +0100

    nix: Fix digital-clock rpath being stripped #BDK-252
    The rpath of digital-clock has been stripped, because patchelf
    is unable to determine that the binary depends on libwaylandclient.so.
    So for now, disable the stripping.
    
    In the future this should be replaced by switching to custom patchelf
    plans that run after the fixup phase so that the rpath is not
    stripped.

--- nix/profiles.nix
+++ nix/profiles.nix
@@ -1,5 +1,13 @@
 # profiles: Build profile definitions for all target platforms.
 { workspaces, pkgs, fixedArmv7Pkgs }:
+let
+  # TODO: replace with patchelf per binary.
+  profileOverlay = profile: (profile // {
+    buildCrate = a: b: (profile.buildCrate a b).overrideAttrs {
+      dontPatchELF = true;
+    };
+  });
+in
 {
   # fast profile (no cross compilation, non-portable binaries)
   fast = workspaces.full.mkBuildProfile {
@@ -22,14 +30,14 @@
     build_pkgs = pkgs.pkgsCross.armv7l-hf-multiplatform.pkgsStatic;
   };
   # glibc profiles for widgets/compositor (dynamically linked)
-  armv7-glibc-release = workspaces.full.mkBuildProfile {
+  armv7-glibc-release = profileOverlay (workspaces.full.mkBuildProfile {
     suffix = "armv7";
     minimal_deps = true;
     rustProfile = "release";
     rustCrossTarget = "armv7-unknown-linux-gnueabihf";
     build_pkgs = fixedArmv7Pkgs;
     wrapNixGL = true;
-  };
+  });
   armv7-glibc-debug = workspaces.full.mkBuildProfile {
     suffix = "armv7";
     minimal_deps = false;
