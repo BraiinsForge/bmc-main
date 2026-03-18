4a543e5834d2bcb98308fdac92b5ddbef7ef3e41
František Boháček <frantisek.bohacek@braiinsforge.com>
2026-03-18 15:28:06 +0100

    nix: Add factory index handling #BDK-356
    - Nix factory index generation for testing

--- nix/lib.nix
+++ nix/lib.nix
@@ -5,9 +5,12 @@ let
     inherit lib;
     autoPatchelfHook = armv7Pkgs.autoPatchelfHook;
   };
+  mkIndex = import ./mkIndex.nix { inherit pkgs lib; };
+  mkTarball = import ./mkTarball.nix { inherit pkgs lib mkIndex; };
+  mkFactoryIndex = import ./mkFactoryIndex.nix { inherit pkgs; };
 in
 {
-  inherit autopatchelfBinaries;
+  inherit autopatchelfBinaries mkIndex mkTarball mkFactoryIndex;
   mkCorePackage = import ./pkgs/core/package.nix { inherit pkgs lib; };
   inherit (import ./widget.nix { inherit pkgs lib autopatchelfBinaries; })
     mkWidgetPackage mkAllWidgets;
