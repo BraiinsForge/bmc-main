666f6fd688d7fd00e9f127a3b718e392de42d133
František Boháček <frantisek.bohacek@braiinsforge.com>
2026-04-08 16:47:05 +0200

    nix: Add bmc-nix-cli to init tarball #BDK-363

--- nix/packages.nix
+++ nix/packages.nix
@@ -27,6 +27,14 @@ in
     upgrade_strategy = "reboot";
     install_strategy = null;
   };
+  bmc-nix-cli = {
+    pkg = profile.buildCrate crates.bmc-nix-cli { };
+    version = "0.1.0";
+    category = "core";
+    description = "Nix package management CLI tool";
+    upgrade_strategy = "reboot";
+    install_strategy = null;
+  };
   nix = {
     pkg = armv7Pkgs.nix;
     version = armv7Pkgs.nix.version;
