2bd683f1788471b905e3a19b6ba66a09a8257647
Josef Kubíček <josef.kubicek@braiins.cz>
2026-05-07 19:51:05 +0300

    nix: workspace: Add alsa-lib to LD_LIBRARY_PATH for nextest #BDK-296
    - rodio (pulled in by bmc-keyboard's keypress sounds) dynlinks
      libasound.so.2 at exec time
    - nextest --list invokes the bmc-storybook test binary and exits 127
      on the loader miss without it

--- workspace.nix
+++ workspace.nix
@@ -137,6 +137,11 @@ let
 
       LD_LIBRARY_PATH = "${lib.makeLibraryPath [
         pkgs.libgcc
+        # rodio (used by bmc-keyboard for keypress sounds, pulled into
+        # bmc-storybook-stories) dynlinks libasound.so.2 at exec time;
+        # nextest invokes test binaries with --list and fails to start
+        # without alsa on the loader path.
+        pkgs.alsa-lib
       ]}";
     };
 
