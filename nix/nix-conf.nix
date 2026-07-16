6dd861cf865f0b401727905f758e0d4a6b858840
František Boháček <frantisek.bohacek@braiinsforge.com>
2026-07-16 14:02:20 +0200

    nix: Add final nix.conf #BDK-567

--- nix/nix-conf.nix
+++ nix/nix-conf.nix
@@ -4,8 +4,8 @@
 # activation entry (pkgs/core/nix-conf-activation.nix).
 { pkgs }:
 pkgs.writeText "nix.conf" ''
-  substituters = https://cache.braiins.com
-  # trusted-public-keys = cache.braiins.com:placeholder
+  substituters = https://downloads.braiinsforge.com
+  trusted-public-keys = downloads.braiinsforge.com-1:4XDOIc61MHtHeIOVrgNOfgzZHt4RCPfqWGDt5PwsLeU=
   extra-experimental-features = nix-command flakes
   # nix-store --realise must fsync realized store path contents (the
   # SQLite DB is fsynced by default, contents are not).
